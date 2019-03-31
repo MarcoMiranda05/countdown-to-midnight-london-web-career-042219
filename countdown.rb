8d35d75677b3f63ea2f1c61014deff5fd6d0e783
Marco Miranda <marmirand@hotmail.com>
2019-03-31 17:10:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
-def countdown(x)
-  while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1
+def countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
